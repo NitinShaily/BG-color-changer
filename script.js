dad7798a1068f9f817982e7acd469272f4e435f7
Nitin Shaily <nitinshaily0@gmail.com>
2021-06-28 01:50:09 +0530

    added user input box

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btton = document.getElementById("click-me");
 const txt = document.getElementById("color-name");
 const bs = document.getElementById('body');
+const form = document.getElementById('inpt');
+const input = document.getElementById('inpt-box');
+
 
 const clrs = ['blue', 'green', 'yellow', 'orange', 'cornsilk', 'red', 'crimson', 'gold', 'gray']
 
@@ -10,3 +13,24 @@ btton.addEventListener('click', (e)=>{
     txt.innerText = pick_color;
     bs.style.backgroundColor = pick_color;
 });
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const pick_color = input.value;
+
+    if(pick_color){
+        if(bs.style.backgroundColor=pick_color){
+            txt.innerText = pick_color;
+            bs.style.backgroundColor = pick_color;
+        }
+        else{
+            alert(pick_color + ' Color not available');
+        }
+    }
+    else{
+        alert('Enter a color');
+    }
+    
+    input.value = '';
+});
+
